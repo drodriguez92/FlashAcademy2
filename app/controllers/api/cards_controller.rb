9d2298d6847502fa01a1075292a87d65ecedb75f
Diego <diego.rodriguez923@gmail.com>
2018-11-21 09:11:56 -0500

    Add deck and card controllers

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,18 +1,77 @@
 class Api::CardsController < ApplicationController
-  def create
-    @user = User.new(user_params)
+  before_action :ensure_logged_in
 
-    if @user.save
-      login(@user)
-      render "api/users/show"
-    else
-      render json: @user.errors.full_messages, status: 422
-    end
-  end
+   def index
+     @cards = Card.all.includes(:author)
+     render :index
+   end
 
-  private
+   def show
+     @card = Card.find(params[:id])
+     render :show
+   end
 
-  def user_params
-    params.require(:user).permit(:username, :password)
-  end
+   def new
+     @card = Card.new
+     render :new
+   end
+
+   def create
+     @card = current_user.cards.new(card_params)
+
+     if @card.save
+       redirect_to card_url(@card)
+     else
+       flash.now[:errors] = @card.errors.full_messages
+       render :new, status: 422
+     end
+   end
+
+   def edit
+     @card = current_user.cards.find(params[:id])
+     render :edit
+   end
+
+   def update
+     card = current_user.cards.find(params[:id])
+
+     if card.update(card_params)
+       redirect_to card_url(card)
+     else
+       flash[:errors] = card.errors.full_messages
+       redirect_to edit_card_url(card)
+     end
+   end
+
+   def destroy
+     card = current_user.cards.find(params[:id])
+     card.destroy
+     redirect_to cards_url
+   end
+
+   def set_cookie
+     maybe_cookie = params[:my_cookie]
+
+     if maybe_cookie
+       cookies[:my_cookie] = maybe_cookie
+       session[:my_cookie] = maybe_cookie
+       render plain: "I am setting your cookie: #{cookies[:my_cookie]}"
+     else
+       render plain: "You must provide a cookie"
+     end
+   end
+
+   def get_cookie
+     my_cookie = cookies[:my_cookie]
+     if my_cookie
+       render plain: "Here's your cookie: #{my_cookie}, and here's your session #{session[:my_cookie]}"
+     else
+       render plain: "You haven't set a cookie called :my_cookie yet"
+     end
+   end
+
+   private
+   def card_params
+     params.require(:card).permit(:question, :answer)
+   end
 end
--- app/controllers/api/decks_controller.rb
+++ app/controllers/api/decks_controller.rb
@@ -1,18 +1,77 @@
 class Api::DecksController < ApplicationController
-  def create
-    @deck = Deck.new(deck_params)
+  before_action :ensure_logged_in
 
-    if @deck.save
-      login(@deck)
-      render "api/decks/show"
-    else
-      render json: @deck.errors.full_messages, status: 422
-    end
-  end
+ def index
+   @decks = deck.all.includes(:author)
+   render :index
+ end
 
-  private
+ def show
+   @deck = deck.find(params[:id])
+   render :show
+ end
 
-  def deck_params
-    params.require(:deck).permit(:username, :password)
-  end
+ def new
+   @deck = deck.new
+   render :new
+ end
+
+ def create
+   @deck = current_user.decks.new(deck_params)
+
+   if @deck.save
+     redirect_to deck_url(@deck)
+   else
+     flash.now[:errors] = @deck.errors.full_messages
+     render :new, status: 422
+   end
+ end
+
+ def edit
+   @deck = current_user.decks.find(params[:id])
+   render :edit
+ end
+
+ def update
+   deck = current_user.decks.find(params[:id])
+
+   if deck.update(deck_params)
+     redirect_to deck_url(deck)
+   else
+     flash[:errors] = deck.errors.full_messages
+     redirect_to edit_deck_url(deck)
+   end
+ end
+
+ def destroy
+   deck = current_user.decks.find(params[:id])
+   deck.destroy
+   redirect_to decks_url
+ end
+
+ def set_cookie
+   maybe_cookie = params[:my_cookie]
+
+   if maybe_cookie
+     cookies[:my_cookie] = maybe_cookie
+     session[:my_cookie] = maybe_cookie
+     render plain: "I am setting your cookie: #{cookies[:my_cookie]}"
+   else
+     render plain: "You must provide a cookie"
+   end
+ end
+
+ def get_cookie
+   my_cookie = cookies[:my_cookie]
+   if my_cookie
+     render plain: "Here's your cookie: #{my_cookie}, and here's your session #{session[:my_cookie]}"
+   else
+     render plain: "You haven't set a cookie called :my_cookie yet"
+   end
+ end
+
+ private
+ def deck_params
+   params.require(:deck).permit(:name, :objective)
+ end
 end
